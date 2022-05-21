80a1ef1b9641b2674785145dcd2597847824768f
disection <diseccionmexico@gmail.com>
2022-05-20 22:16:30 -0500

    Add inicial comit class/class-four

--- js/ciclos.js
+++ js/ciclos.js
@@ -7,10 +7,10 @@
 
 */ 
 
-for(let i = 1; i<= 10; i++){
-    // sentecias
-    console.log(`5 x ${i}`)
-}
+// for(let i = 1; i<= 10; i++){
+//     // sentecias
+//     console.log(`5 x ${i}`)
+// }
 //------------ solución 1
 // let str = 'Hola koders' // el string cuenta de 0 ... n en este aso te arroja 11
 // let end = str.length - 1 //en length cuenta de 1 ... n en este caso te arroja 12
@@ -18,11 +18,13 @@ for(let i = 1; i<= 10; i++){
 //     console.log(str[i])
 // }
 //------------ solución 2
-let str = 'Hola koders' 
-let end = str.length // no le restamos nada
-for(let i = 0; i< end; i++){ //la equivalencia es solo < menor que
-    console.log(str[i])
-}
+
+
+// let str = 'Hola koders' 
+// let end = str.length // no le restamos nada
+// for(let i = 0; i< end; i++){ //la equivalencia es solo < menor que
+//     console.log(str[i])
+// }
 
 
 // "Hola Koder"
@@ -34,52 +36,102 @@ for(let i = 0; i< end; i++){ //la equivalencia es solo < menor que
 // o
 // e
 
-for(let i = 0; i < end; i++){
-    let caracter = str[i]
-    // console.log(caracter)
-    if(
-        caracter == 'a' || 
-        caracter == 'e' || 
-        caracter == 'i' || 
-        caracter == 'o' || 
-        caracter == 'u'
-    ){
-        console.log(caracter)
-    }
-} 
+// for(let i = 0; i < end; i++){
+//     let caracter = str[i]
+//     // console.log(caracter)
+//     if(
+//         caracter == 'a' || 
+//         caracter == 'e' || 
+//         caracter == 'i' || 
+//         caracter == 'o' || 
+//         caracter == 'u'
+//     ){
+//         console.log(caracter)
+//     }
+// } 
 
 
 // "Hola Koder"
 // Imprimir en consola cada una de las consonantes
 // ciclos y condicionales
 
-for(let i = 0; i < end; i++){
-    let caracter = str[i]
-    // console.log(caracter)
-    if(
-        caracter != 'a' && 
-        caracter != 'e' && 
-        caracter != 'i' && 
-        caracter != 'o' && 
-        caracter != 'u' &&
-        caracter != ' '
-    ){
-        console.log(caracter)
-    }
-} 
+// for(let i = 0; i < end; i++){
+//     let caracter = str[i]
+//     // console.log(caracter)
+//     if(
+//         caracter != 'a' && 
+//         caracter != 'e' && 
+//         caracter != 'i' && 
+//         caracter != 'o' && 
+//         caracter != 'u' &&
+//         caracter != ' '
+//     ){
+//         console.log(caracter)
+//     }
+// } 
 
-for(let i = 0; i < end; i++){
-    let caracter = str[i]
-    // console.log(caracter)
-    if (!(
-        caracter == 'a' || 
-        caracter == 'e' || 
-        caracter == 'i' || 
-        caracter == 'o' || 
-        caracter == 'u'
-    )
-    ){
-        console.log(caracter)
-    }
-} 
+// for(let i = 0; i < end; i++){
+//     let caracter = str[i]
+//     // console.log(caracter)
+//     if (!(
+//         caracter == 'a' || 
+//         caracter == 'e' || 
+//         caracter == 'i' || 
+//         caracter == 'o' || 
+//         caracter == 'u'
+//     )
+//     ){
+//         console.log(caracter)
+//     }
+// } 
 
+// let counter = 1
+// while( counter <= 10 ) {
+//     console.log(` 5 x ${counter} = ${ 5 * counter } `)
+//     counter++
+// }
+
+// imprimir cada palabra de un string -> pasar a while
+// let str = 'Hola Koder'
+// let i= 0
+// while ( i < str.length ) {
+//     console.log( str[i] )
+//     i++
+// }
+
+/**
+ * 'Hola Koder'
+ * -> 'redok aloh'
+ * Imprimir el string invertido
+ * 
+ * 
+ */ 
+
+
+/**
+ * 'oaoe'
+ * Imprimir en consola cada una de las vocales
+ * 
+ */ 
+let str = 'Hola Koders'
+let i = 0
+while ( i < str.length) {
+    if (
+          str == 'a' || 
+          str == 'e' || 
+          str == 'i' || 
+          str == 'o' || 
+          str == 'u' ||
+          str == ' '
+             
+        ){
+             console.log(str[i])
+        }   
+    i++
+}
+
+/**
+ * 'HlKdr'
+ * Imprimir en consola cada una de las consonantes
+ * 
+ */ 
