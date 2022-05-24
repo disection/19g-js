087032629204741d145a4af7d9d1c77d23fe5f9c
disection <diseccionmexico@gmail.com>
2022-05-24 11:39:33 -0500

    homework class-five

---
+++ js/homework.js
@@ -0,0 +1,54 @@
+/**
+ * Funcion que pida una palabra al usuario
+ * Invierta la palabra
+ * y la retorne en un alert
+ * reversedString('hola') -> 'aloh' 
+ */
+
+
+/**
+ * Funcion que pida 3 numeros como parametro
+ * 2 numeros obligatorios y 1 opcional con valor de 3
+ * Arroje la suma de esos 3 numeros
+ * Posibles resultados
+ * -> addThreeNumbers(3,4,5) -> 12
+ * -> addThreeNumbers(3,4) -> 10
+ * -> addThreeNumbers(3) -> 'Faltan datos'
+ * -> addThreeNumbers() -> 'Faltan datos'
+ */
+
+
+/**
+ * Funcion que pida al usuario los grados Centigrados en su localidad
+ * Convertirlos a grados fahrenheit
+ * Arrojar un alert con el resultado
+ * -> convertTemp(25) -> La temperatura es de 77 ºF
+ * -> convertTemp() -> 'Faltan datos'
+ */
+
+
+
+/**
+ * Funcion que pida al usuario los grados Centigrados en su localidad
+ * Convertirlos a grados fahrenheit
+ * Arrojar un alert con el resultado
+ * -> convertTemp(25) -> La temperatura es de 77 ºF
+ * -> convertTemp() -> 'Faltan datos'
+ */
+
+
+
+/**
+ * Funcion que imprima las tablas del 1 al 10
+ */
+
+
+
+/**
+ * Funcion que pida al usuario un numero (N) entre 1 y 100 
+ * Mandar un alert con suma de 1 a N
+ * Valor por defecto 3
+ * addNumberLimit() -> 6
+ * addNumberLimit(4) -> 10
+ * addNumberLimit(5) -> 15
+ */
