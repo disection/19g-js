278c770aa6d88a36f61d7cbc3b3c67d7b2b56355
disection <diseccionmexico@gmail.com>
2022-05-24 16:42:36 -0500

    repaso literales

---
+++ js/literals.js
@@ -0,0 +1,9 @@
+// backtick (`) characters -> multi-line strings, string interpoletion, tagged templates
+//Syntax
+
+// Untagged, these create strings:
+`string text`
+`string text ${expression} more text` //lo que esta entre corchetes (placeholders) es el argumento de una función
+
+// multi-line string
+console.log('string line one' + 'string line two')
