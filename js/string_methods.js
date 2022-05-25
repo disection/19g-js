4b5f975f2d3c7080b897d8afd2b7892fa4858df2
disection <diseccionmexico@gmail.com>
2022-05-25 18:27:00 -0500

    Change string_methods

--- js/string_methods.js
+++ js/string_methods.js
@@ -18,4 +18,23 @@ let sentence = "Hola koders!";
 // asignamos a una variable el número de carácteres de  sentence
 let len = sentence.length;
 
-console.log(len);
+console.log( len );
+
+// String 
+let nombre ='Lorena'
+let apellido = "Terrazas"
+let saludo = new String('hola mundo')
+let lorem = ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam rem magni accusantium repudiandae!'
+
+console.group( nombre, apellido, saludo)
+// propiedades
+nombre.length //longitud del string
+apellido.toLocaleLowerCase
+nombre.toLocaleUpperCase
+lorem.includes('amet')
+lorem.includes('lorena') 
+lorem.trim() //quita los espacios en blanco al inico y al final
+lorem.split(' ') // separa una cadena en arreglos partiendo del parametro que se indique en el método
+
+
+// métodos
