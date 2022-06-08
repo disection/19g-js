5196887ac5e8bb3c5bbbcba0db230cca433e5114
disection <diseccionmexico@gmail.com>
2022-06-08 10:17:16 -0500

    ADD initial commit

---
+++ js/bom.js
@@ -0,0 +1,36 @@
+//DOM
+// Document Object Model ->
+// Manipular el documento html ->
+// document.querySelector('item') ->
+// element.classlist.add('clasenueva') ->
+// element.innerHTML = 'nuevohtml' ->
+// element.addAtrubute('propiedad', 'valor') ->agregar atributo
+// element.getAttribute ('propiedad') ->
+// document.getElemtsByTagName ('h2')[1] -> agregar un elemento mediante el tag
+// document.createElement ('h3') ->
+// document.getElemenById ()->
+// ->
+
+// Browser Object Model (BOM)
+// Comunicarse con el navegador
+
+// viewport size
+// window
+// window.innerWidth -> lo que se ouede ver en pantalla el ancho viewport
+// window.innerHeight -> lo que se ouede ver en pantalla el alto viewport
+
+// window.screem -> medidas de la pantalla reales
+
+// window.navigator -> toda la información del navegador detalles
+
+// window.history.back() ->  hacia la página anterior
+// window.history.forward() -> hacia la siguiente página abierta
+
+// window.location -> todo lo que esta en la barra de direcciones 
+ // ESTUDIAR LAS PROPIEDADES ******
+
+ // AJAX
+ // ¿qué es?
+ // diferentes formas de implementarlo
+
+
