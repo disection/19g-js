c33b7dda88b735b751498c27635bba8cca6236de
disection <diseccionmexico@gmail.com>
2022-06-10 14:09:52 -0500

    Add excersice ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,98 +1,83 @@
-// Ajax
-// Asyncronic JavaScrip and XML
-// XMLHttpRequest
-
-/**
- * 0: request not initialized
- * 1:server connetcion establishet
- * 2: request reci
- * 3:
- * 4:
- */
-
-// formar request 
-// enviar request
-// esperar respuesta
-// recibir response
-// procesar el response
-
-let users = []
-// create an XMLHttpRequest object
-
-// function imprime en layout la lista de usuarios
-  
-  const printUserList = (usersFiltered) => {
-    let allUserLayout = usersFiltered.reduce((acc, user, idx, arr) => {
-      return acc += `
-      <a href="#" class="list-group-item list-group-item-action " aria-current="true">
-        <div class="d-flex w-100 justify-content-between">
-          <h5 class="mb-1">${user.name}</h5>
-        </div>
-        <p class="mb-1">${user.phone}</p>
-        <small>${user.email}</small>
-      </a>
-      `
-    }, '')
-  
-    document.getElementById('users__filtered').innerHTML = allUserLayout
-  }
-
-const primerPeticion = new XMLHttpRequest()
-console.log( primerPeticion )
-
-// Define a callback function
-primerPeticion.onload = (userLoad) => {
-    // here you can use the Data
-    // console.log(userLoad)
-    if (userLoad.target.status >= 200 && userLoad.target.status <= 299 ) {
-        //console.log(userLoad)
-        //console.log(userLoad.target.responseText)
-
-        // JSON
-        // metodos.parse( text - object) .stringify(obj - text)
-        let responseJSON = JSON.parse(userLoad.target.responseText)
-        users = responseJSON 
-        //console.log(users)
-        printUserList(users)
-        
-    }
-}
-
-// Send a request
-// GET, POST, DELETE, PATCH, PUT
-primerPeticion.open( 'GET', 'https://jsonplaceholder.typicode.com/users', false )
-console.log(primerPeticion)
-primerPeticion.send()
-
-
-// se filtra a partir del string
-const filterUsers = ( strToFilter ) => {
-    strToFilter = strToFilter.toLowerCase()
-    let filteredUsers = users.filter( (user) => {
-      if(
-        user.name.toLowerCase().includes(strToFilter) ||
-        user.username.toLowerCase().includes(strToFilter) ||
-        user.email.toLowerCase().includes(strToFilter)
-      ){
-        return user
-      }
-    })
-    return filteredUsers
-  }
+// VERBOS http ->
+// payload
+// JSON.parse(str) -> obj
+// JSON.stringify(obj) -> obj
 
  
-  
-  // cuando cargue la pagina, cargar todos los usuarios
-let searchBox = document.querySelector('#filter__user')
-searchBox.addEventListener('keyup',  ()=>{
-    printUserList(filterUsers(searchBox.value))
-} )
-  
-  
+// Create POST
+// Read GET
+// Update 
+//   PATCH (actualiza solo un fragmento o todo) 
+//   PUT (actualiza todo)
+// Delete DELETE
+
+const createUser =  ( objUser ) => {
+  console.log(JSON.stringify(objUser))
+  const xhttp = new XMLHttpRequest()
+  xhttp.open( "POST",  
+    "https://lorekoderjs19-default-rtdb.firebaseio.com/users/.json", true)
+  xhttp.onload = function(data) {
+    console.log(data)
+      if(data.target.status >= 200 && data.target.status <= 399){
+          console.log(data.target.response)
+      }
+  }
+  xhttp.send( JSON.stringify(objUser) )
+}
+
+// createUser( user )
+
+// let user = {
+//   name : 'Carol',
+//   lastName : 'Savedra',
+//   age : '38'
+// }
+
+const updateUser = ( idUser, userUpdated ) => {
+  const updatexhr = new XMLHttpRequest( )
+  updatexhr.open('PATCH',`https://lorekoderjs19-default-rtdb.firebaseio.com/users/-N4AKDmEe6JRBlC4yxH0.json`,true)
+
+  updatexhr.onload = (response) => {
+    if(response.target.status >= 200 && response.target.status <= 399 ){
+      console.log(response.target)
+      console.log(response.target.response)
+    }
+  }
+  updatexhr.send( JSON.stringify(userUpdated) )
+}
 
 
+// let objUpdate = {
+//   name : 'Carol',
+//   lastName : 'Savedra',
+//   age : '38'
+// }
 
+const updateUserPUT = ( userUpdated ) => {
+  const updatexhr = new XMLHttpRequest( )
+  updatexhr.open('PUT','https://lorekoderjs19-default-rtdb.firebaseio.com/users/-N4AKDmEe6JRBlC4yxH0.json',true)
 
+  updatexhr.onload = (response) => {
+    if(response.target.status >= 200 && response.target.status <= 399 ){
+      console.log(response.target)
+      console.log(response.target.response)
+    }
+  }
+  updatexhr.send( JSON.stringify(userUpdated) )
+}
 
-
-
+const deleteUser  = ( idUser ) => {
+  const deletexhr = new XMLHttpRequest()
+  deletexhr.open(
+    'DELETE', 
+    `https://koders19gjs-default-rtdb.firebaseio.com/users/${idUser}.json`, 
+    true
+  )
+  deletexhr.onload = (response) => {
+    if(response.target.status >= 200 && response.target.status <= 399){
+      console.log(response.target)
+      console.log(response.target.response)
+    }
+  }
+  deletexhr.send()
+}
